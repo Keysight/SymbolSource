51c23a9f112b3d66ad3897b48808d7bd5f7b7de1
Scott Selberg <scott_selberg@keysight.com>
2019-10-25 11:36:11 -0600

    Added a windows service project

---
+++ SymbolSource.Processor.WindowsService/SymbolSourceProcessorWindowsService.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics;
+using System.ServiceProcess;
+using System.Threading;
+using Autofac;
+using Microsoft.Azure.WebJobs;
+using Microsoft.ApplicationInsights.Extensibility;
+using SymbolSource.Contract;
+using SymbolSource.Contract.Container;
+using SymbolSource.Contract.Scheduler;
+using SymbolSource.Support;
+
+namespace SymbolSource.Processor.WindowsService
+{
+   public partial class SymbolSourceProcessorWindowsService : ServiceBase
+   {
+      CancellationTokenSource cancelSource;
+
+      public SymbolSourceProcessorWindowsService()
+      {
+         InitializeComponent();
+         this.cancelSource = new CancellationTokenSource();
+      }
+
+      protected override void OnStart(string[] args)
+      {
+         Trace.Listeners.Add(new ConsoleTraceListener());
+
+         foreach (var assembly in typeof(PackageProcessor).Assembly.GetReferencedAssemblies())
+            Trace.WriteLine(assembly.FullName);
+
+         var shutdownWatcher = new WebJobsShutdownWatcher();
+         var shutdownSource = CancellationTokenSource.CreateLinkedTokenSource(new[]
+         {
+                shutdownWatcher.Token,
+                cancelSource.Token
+         });
+
+         var configuration = new DefaultConfigurationService();
+         var builder = new ContainerBuilder();
+
+         DefaultContainerBuilder.Register(builder, configuration);
+         SupportContainerBuilder.Register(builder, SupportEnvironment.WebJob);
+         PackageProcessorContainerBuilder.Register(builder);
+
+         var container = builder.Build();
+
+         var support = container.Resolve<ISupportConfiguration>();
+
+         if (!string.IsNullOrWhiteSpace(support.InsightsInstrumentationKey))
+            TelemetryConfiguration.Active.InstrumentationKey = support.InsightsInstrumentationKey;
+
+         var scheduler = container.Resolve<ISchedulerService>();
+         scheduler.ListenAndProcess(shutdownSource.Token);
+      }
+
+      protected override void OnStop()
+      {
+            this.cancelSource.Cancel();
+      }
+   }
+}
